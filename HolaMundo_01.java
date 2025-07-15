de726de1f6b5e6a14d4f6123625dcdf6c10b9b8f
almanterok <fernandoserrao28@gmail.com>
2025-07-15 23:18:44 +0200

    Add files via upload
    mi primer programa de "Hola Mundo"

---
+++ HolaMundo_01.java
@@ -0,0 +1,11 @@
+package es.ProgramasInstagram.Diario;
+
+public class HolaMundo_01 {
+
+	public static void main(String[] args) {
+		// Vamos a imprmir por pantalla un HOLA MUNDO
+		System.out.println("Hola Mundo");
+
+	}
+
+}
